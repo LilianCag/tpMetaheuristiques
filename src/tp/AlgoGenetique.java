ca1730e95e7dfbb1b4ba63219cacd63908be49b7
Arthur <arthurquique@protonmail.com>
2020-12-16 11:49:54 +0100

    Lecture fichier by Yassine

--- src/tp/AlgoGenetique.java
+++ src/tp/AlgoGenetique.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 public class AlgoGenetique {
 
     private int population;
+    private double taux_meilleurs;
     private double taux_mutation;
     private int nb_generations;
 
-    public AlgoGenetique(int population, double taux_mutation, int nb_generations) {
+    public AlgoGenetique(int population, double taux_meilleurs, double taux_mutation, int nb_generations) {
         this.population = population;
+        this.taux_meilleurs = taux_meilleurs;
         this.taux_mutation = taux_mutation;
         this.nb_generations = nb_generations;
     }
@@ -29,6 +31,10 @@ public class AlgoGenetique {
     public Individu[] selection(Individu[] individus) {
         Arrays.sort(individus, (a,b) -> b.getScore() - a.getScore());
         Individu[] selectionIndividus = new Individu[this.population/2];
+        int i;
+        for(i=0; i<Math.floor(this.population*taux_meilleurs); i++) {
+            selectionIndividus[i] = individus[i];
+        }
         return selectionIndividus;
     }
 
--- src/tp/Test.java
+++ src/tp/Test.java
@@ -1,15 +1,61 @@
 package tp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
+        String FichierLire = "sdkp2_7.txt";
+        int n; // nombre de groupes
+        int b; // capacité du sac
+        Item tabx[]; // tableau d'objets
+        int opt_value = 0; // différent de 0 après lecture du fichier si la valeur optimale est fournie
+
+        try {
+            File myObj = new File("src/Instances_DKP/" + FichierLire);
+            Scanner myReader = new Scanner(myObj);
+            int line = 1;
+            int nextLine = 0;
+            int compteur = 0;
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (line == 1) {
+                    //premier valeur
+                    nextLine = line + 1;
+                }
+                if (line == 2) {
+                    //deuxieme Valeur
+                    nextLine = line + 1;
+                }
+                if (line == 3 || line == 4 || line == 13 || line == 14) {
+                    String[] tab = data.split(" ");
+                    if (tab.length == 1) {
+                        //La Troisieme Valeur
+                        nextLine = line + 10;
+                    }
+
+                }
+                System.out.println(data);
+                line = nextLine;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+
         /*
          * Variables d'environnement
          */
         int population = 100;
+        double taux_meilleurs = 0.2;
         double taux_mutation = 0.01;
         int nb_generations = 1000;
-        AlgoGenetique ag = new AlgoGenetique(population, taux_mutation, nb_generations);
+        AlgoGenetique ag = new AlgoGenetique(population, taux_meilleurs, taux_mutation, nb_generations);
         ag.algo();
     }
 }
